05aecea61e19eb947f94df7cefce5bbdd8b48dbb
emmathedata <88256051+emmathedata@users.noreply.github.com>
2022-05-09 12:43:13 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr_blocks" {}
+variable "subnet_cidr_blocks" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key" {}
+variable "entry_script" {}
+variable "image_name" {}
